09811b5ed98cda4c3611a47e616c5dcb846c77ff
cais <cais@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-16 22:07:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/bns-login/trunk@146458 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bns-login.php
+++ bns-login.php
@@ -1,9 +1,10 @@
 <?php
+/*<?php
 /*
 Plugin Name: BNS Login
 Plugin URI: http://buynowshop.com/
 Description: A simple plugin providing a link to the dashboard; and, a method to log in and out of your blog in the footer of the theme. This is ideal for those not wanting to use the meta widget/code links. 
-Version: 1.0.1
+Version: 1.1
 Author: Edward "cais" Caissie
 Author URI: http://edwardcaissie.com/
 */
@@ -12,7 +13,13 @@ function bns_addlogin() {
   $login_url = get_bloginfo('url') . '/wp-admin/';
   if ( is_user_logged_in() ) {
     echo '<div id="bns-logged-in" class="bns-login">' . 'You are logged in! ';
-    echo '<a href="' . wp_logout_url( get_bloginfo('url') ) . '" title="Logout">Logout</a>';
+    if (function_exists(get_current_site)) {
+      $current_site = get_current_site();
+      $home_domain = 'http://' . $current_site->domain . $current_site->path;
+      echo '<a href="' . wp_logout_url( $home_domain ) . '" title="Logout">Logout</a>';
+    } else {
+      echo '<a href="' . wp_logout_url( get_bloginfo('url') ) . '" title="Logout">Logout</a>';
+    }
     echo ' or go to the <a href="' . $login_url . '" title="dashboard">dashboard</a>.' . '</div>';
   } else {
     echo '<div id="bns-logged-out" class="bns-login">' . '<a href="' . $login_url . '" title="Log in here">Log in here!</a>' . '</div>';
