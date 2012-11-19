d383c1bf6fff4d7d47ead51297f0764c1ca97a88
cais <cais@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-19 20:24:26 +0000

    * Add empty hooks before and after main output * Add filter hooks to all of the text output * Add Shortcode functionality to text widgets * Add Shortcode for this plugin * Add wrapping classes around output elements * Refactored `bns_login_main` to use hooks instead of array elements * Remove `load_textdomain` as redundant * Updated 'readme' with FAQ on shortcode use
    git-svn-id: https://plugins.svn.wordpress.org/bns-login/trunk@627269 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bns-login.php
+++ bns-login.php
@@ -3,7 +3,7 @@
 Plugin Name: BNS Login
 Plugin URI: http://buynowshop.com/plugins/bns-login/
 Description: A simple plugin providing a link to the dashboard; and, a method to log in and out of your blog in the footer of the theme. This is ideal for those not wanting to use the meta widget/code links.
-Version: 1.9
+Version: 2.0
 Text Domain: bns-login
 Author: Edward Caissie
 Author URI: http://edwardcaissie.com/
@@ -21,7 +21,7 @@ License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * @link        http://buynowshop.com/plugins/bns-login/
  * @link        https://github.com/Cais/bns-login/
  * @link        http://wordpress.org/extend/plugins/bns-login/
- * @version     1.9
+ * @version     2.0
  * @author      Edward Caissie <edward.caissie@gmail.com>
  * @copyright   Copyright (c) 2009-2012, Edward Caissie
  *
@@ -45,43 +45,35 @@ License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * The license for this software can also likely be found here:
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
- * @version 1.9
- * @date    September 18, 2012
- * Implement OOP style class coding
+ * @version 2.0
+ * @date    November 19, 2012
+ * Add empty hooks before and after main output
+ * Add filter hooks to all of the text output
+ * Add Shortcode functionality to text widgets
+ * Add Shortcode for this plugin
+ * Add wrapping classes around output elements
+ * Refactored `bns_login_main` to use hooks instead of array elements
+ * Remove `load_textdomain` as redundant
+ * Updated 'readme' with FAQ on shortcode use
  */
 
 class BNS_Login {
-    /** Constructor */
+    /**
+     * Constructor
+     *
+     * @package BNS_Login
+     * @since   1.9
+     *
+     * @uses    (global) wp_version
+     * @uses    add_action
+     * @uses    add_shortcode
+     */
     function __construct(){
 
-        /**
-         * BNS Login TextDomain
-         * Make plugin text available for translation (i18n)
-         *
-         * @package:    BNS_Login
-         * @since:      1.8
-         *
-         * @internal    Note: Translation files are expected to be found in the plugin root folder / directory.
-         * @internal    See Text Domain: bns-login
-         */
-        load_plugin_textdomain( 'bns-login' );
-
-        /**
-         * Check installed WordPress version for compatibility
-         *
-         * @package     BNS_Login
-         * @since       1.0
-         *
-         * @uses        (global) wp_version
-         *
-         * @internal    Version 3.0 being used in reference to home_url()
-         *
-         * @version     1.8
-         * Last revised November 22, 2011
-         * Re-write to be i18n compatible
-         */
+        /** Check installed WordPress version for compatibility */
         global $wp_version;
         $exit_ver_msg = __( 'BNS Login requires a minimum of WordPress 3.0, <a href="http://codex.wordpress.org/Upgrading_WordPress">Please Update!</a>', 'bns-login' );
+        /** Version 3.0 is used for `home_url` */
         if ( version_compare( $wp_version, "3.0", "<" ) ) {
             exit ( $exit_ver_msg );
         }
@@ -90,7 +82,12 @@ class BNS_Login {
         add_action( 'wp_enqueue_scripts', array( $this, 'Scripts_and_Styles' ) );
 
         /** Add BNS Login to Footer */
-        add_action( 'wp_footer', array( $this, 'Add_BNS_Login' ) );
+        add_action( 'wp_footer', array( $this, 'bns_login_output' ) );
+
+        /** Add Shortcode functionality to text widgets */
+        add_action( 'widget_text', 'do_shortcode' );
+        /** Add Shortcode for this plugin */
+        add_shortcode( 'bns_login', array( $this, 'bns_login_main' ) );
 
     }
 
@@ -125,8 +122,6 @@ class BNS_Login {
      * @package BNS_Login
      * @since   0.1
      *
-     * @param   string $args
-     *
      * @uses    apply_filters
      * @uses    get_current_site
      * @uses    home_url
@@ -136,11 +131,13 @@ class BNS_Login {
      * @uses    wp_register
      *
      * @return  mixed|string|void
+     *
+     * @version 2.0
+     * @date    November 19, 2012
+     * Add wrapping classes around output elements
+     * Refactored to use filters instead of array elements
      */
-    function Main( $args = '' ) {
-        $values = array( 'login' => '', 'after_login' => '', 'logout' => '', 'goto' => '', 'separator' => '' );
-        $args = wp_parse_args( $args, $values );
-
+    function bns_login_main() {
         /** Initialize $output - start with an empty string */
         $output = '';
         /**
@@ -152,56 +149,65 @@ class BNS_Login {
          * @var $separator      string - characters used to separate link/message texts
          * @var $sep            string - $separator wrapper for styling purposes, etc. - just in case ...
          */
-        $login        = empty( $args['login'] ) ? sprintf( __( 'Log in here!', 'bns-login' ) ) : $args['login'];
-        $after_login  = empty( $args['after_login'] ) ? sprintf( __( 'You are logged in!', 'bns-login' ) ) : $args['after_login'];
-        $logout       = empty( $args['logout'] ) ? sprintf( __( 'Logout', 'bns-login' ) ) : $args['logout'];
-        $goto         = empty( $args['goto'] ) ? sprintf( __( 'Go to Dashboard', 'bns-login' ) ) : $args['goto'];
-        $separator    = empty( $args['separator'] ) ? sprintf( __( ' &deg;&deg; ' ) ) : $args['separator'];
-        $sep          = '<span class="bns-login-separator">' . $separator . '</span>';
+        $login          = apply_filters( 'bns_login_here',          sprintf( __( 'Log in here!', 'bns-login' ) ) );
+        $after_login    = apply_filters( 'bns_login_after_login',   sprintf( __( 'You are logged in!', 'bns-login' ) ) );
+        $logout         = apply_filters( 'bns_login_logout',        sprintf( __( 'Logout', 'bns-login' ) ) );
+        $goto           = apply_filters( 'bns_login_goto',          sprintf( __( 'Go to Dashboard', 'bns-login' ) ) );
+        $separator      = apply_filters( 'bns_login_separator',     sprintf( __( ' &deg;&deg; ' ) ) );
+        $sep            = apply_filters( 'bns_login_sep',           '<span class="bns-login-separator">' . $separator . '</span>' );
+        $login_url      = apply_filters( 'bns_login_url',           home_url( '/wp-admin/' ) );
 
         /** The real work gets done next ...  */
-        $login_url = home_url( '/wp-admin/' );
         if ( is_user_logged_in() ) {
-            $output .= '<div id="bns-logged-in" class="bns-login">' . $after_login . $sep;
+            $output .= '<div id="bns-logged-in" class="bns-login">' . '<span class="bns-after-login">' . $after_login . '</span>' . $sep;
             /** Multisite - logout returns to Multisite main domain page */
             if ( function_exists( 'get_current_site' ) ) {
                 $current_site = get_current_site();
-                /** @noinspection PhpUndefinedFieldInspection */
+                /** @var $home_domain - constructed url */
                 $home_domain = 'http://' . $current_site->domain . $current_site->path;
                 $logout_url = wp_logout_url( $home_domain );
             } else {
                 $logout_url = wp_logout_url( home_url() );
             }
-            $output .= '<a href="' . $logout_url . '" title="' . $logout . '">' . $logout . '</a>' . $sep;
-            $output .= '<a href="' . $login_url . '" title="' . $goto . '">' . $goto . '</a></div>';
+            $output .= '<a class="bns-logout-url" href="' . $logout_url . '" title="' . $logout . '">' . $logout . '</a>' . $sep;
+            $output .= '<a class="bns-login-url" href="' . $login_url . '" title="' . $goto . '">' . $goto . '</a></div>';
         } else {
-            /** if user is not logged in display login; or, register if allowed */
+            /** Display login message */
             $output .= '<div id="bns-logged-out" class="bns-login">';
-            $output .= '<a href="' . $login_url . '" title="' . $login . '">' . $login . '</a>';
+            $output .= '<a class="bns-login-url" href="' . $login_url . '" title="' . $login . '">' . $login . '</a>';
+            /** Show register link if new users allowed in site settings */
             $output .= wp_register( $sep, '', false );
             $output .= '</div>';
         }
-        $output = apply_filters( 'Main', $output, $args );
 
         return $output;
+
     }
 
     /**
      * Add BNS Login
+     * Echos the Main function output
      *
      * @package BNS_Login
-     * @since 1.0
+     * @since   1.0
      *
-     * Uses the following parameters (see defaults in BNS_Login function)
-     *    login       => anchor text to login URL
-     *    after_login => message showing end-user is logged in
-     *    logout      => anchor text to logout URL
-     *    goto        => anchor text to dashboard / Administration Panels
-     *    separator   => character(s) used to separate the anchor texts
+     * @uses    bns_login_main
+     * @uses    do_action
+     *
+     * @version 2.0
+     * @date    November 6, 2012
+     * Removed parameters - see changes to `bns_login_main`
+     * Add empty hooks before and after main output
      */
-    function Add_BNS_Login() {
-        /** BNS_Login pre-populated with empty parameters as guidelines */
-        echo $this->Main( 'login=&after_login=&logout=&goto=&separator=' );
+    function bns_login_output() {
+        /** Add empty hook before output */
+        do_action( 'bns_login_before_output' );
+
+        /** Output to screen */
+        echo $this->bns_login_main();
+
+        /** Add empty hook after output */
+        do_action( 'bns_login_after_output' );
     }
 
 }
